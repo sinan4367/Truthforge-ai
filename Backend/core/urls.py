06f9433290cbeb405d582d13355075534e5bbc4e
Yazir M <yazirmusthafa1@gmail.com>
2025-09-02 19:42:28 +0530

    back end update with poisoning need high gpu

--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -4,4 +4,6 @@ from .views import UploadAndGroupView
 
 urlpatterns = [
     path('api/upload/', UploadAndGroupView.as_view(), name='upload-and-group'),
+    path('generate', generate, name='generate'),
+    path('poison', poison, name='poison'),
 ]
