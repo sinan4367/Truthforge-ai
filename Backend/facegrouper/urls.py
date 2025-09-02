2dd79b5edabb5b38ba6dec1f84f48a4510aee8e5
Yazir M <yazirmusthafa1@gmail.com>
2025-09-02 21:27:21 +0530

    updated but now we need high gpu power today final

--- Backend/facegrouper/urls.py
+++ Backend/facegrouper/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')), 
-     path('api/', include('api.urls')), # include core app urls
+     path('api/', include('api.urls')),
 ]
 
 # Serve media/cache files in development
