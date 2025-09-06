a57e5c32002c115997a641620b9a0400c922426d
Yazir M <yazirmusthafa1@gmail.com>
2025-09-06 18:41:50 +0530

    added security with blockchain against data injection

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,10 +1,14 @@
 # backend/api/urls.py
 from django.urls import path
-from .views import compare_poisoned, generate, poison, revert_poison
+from .views import *
 
 urlpatterns = [
     path('generate', generate, name='generate'),
     path('poison', poison, name='poison'),
     path("revert_poison", revert_poison), 
      path("compare_poisoned", compare_poisoned),  
+     path("generate_blockchain", generate_blockchain),
+    path("poison_blockchain", poison_blockchain),
+    path("revert_blockchain", revert_blockchain),
+# path("get_blockchain", get_blockchain_blocks),
 ]
